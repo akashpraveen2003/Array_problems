f1dfff26695f3ea80f0bbbb8ccdca05eb79b8bb7
akashpraveen2003 <82281121+akashpraveen2003@users.noreply.github.com>
2023-07-13 17:46:49 +0530

    Update Max_min_in_array.java

--- src/basic_array_problems_1/Max_min_in_array.java
+++ src/basic_array_problems_1/Max_min_in_array.java
@@ -3,10 +3,22 @@ package basic_array_problems_1;
 import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+Given an array A of size N. You need to find the sum of Maximum and Minimum element in the given array.
+
+Input
+	A = [-2, 1, -4, 5, 3]
+Output
+	1
+
+Input
+	A = [1, 3, 4, 1]
+Output
+	5
+*/
+
 public class Max_min_in_array {
 	private static int sum_of_max_min(int[] array) {
-		// TODO Auto-generated method stub
-		
 		Arrays.sort(array);
 		if(array.length==1)
 		{
