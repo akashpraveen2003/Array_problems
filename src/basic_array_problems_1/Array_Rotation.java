65b0baeb57d702861490a06d55726278554cd89c
akashpraveen2003 <82281121+akashpraveen2003@users.noreply.github.com>
2023-07-13 17:41:12 +0530

    Array_Rotation.java

--- src/basic_array_problems_1/Array_Rotation.java
+++ src/basic_array_problems_1/Array_Rotation.java
@@ -3,6 +3,23 @@ package basic_array_problems_1;
 import java.util.Arrays;
 import java.util.Scanner;
 
+/**
+Given an integer array A of size N and an integer B, you have to return the same array after rotating it B times towards the right.
+Test Cases:
+Input
+A = [1, 2, 3, 4]
+B = 2
+
+Output
+[3, 4, 1, 2]
+Input
+A = [2, 5, 6]
+B = 1
+
+Output
+[6, 2, 5]
+*/
+
 public class Array_Rotation {
 
 	private static void reverse(int[] array, int start, int end) {
