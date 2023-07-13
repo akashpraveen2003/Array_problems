e47ab287809830c98ef74edf901f22a56e881acb
akashpraveen2003 <82281121+akashpraveen2003@users.noreply.github.com>
2023-07-13 17:43:12 +0530

    Count_elements_greater_than_itself.java

--- src/basic_array_problems_1/Count_elements_greater_than_itself.java
+++ src/basic_array_problems_1/Count_elements_greater_than_itself.java
@@ -1,7 +1,22 @@
 package basic_array_problems_1;
 
 import java.util.Scanner;
+/**
+Given an array A of 'n' integers. 
+Count the number of elements that have at least 1 elements greater than itself.
 
+Test Cases:
+Input
+A = [3, 1, 2]
+
+Output
+2
+Input
+A = [5, 5, 3]
+
+Output
+1
+*/
 public class Count_elements_greater_than_itself {
 
 	private static int Count_elements(int array[],int size)
