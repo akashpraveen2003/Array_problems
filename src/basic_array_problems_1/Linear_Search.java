74fc017682f09130183901c9f5570858492321bc
akashpraveen2003 <82281121+akashpraveen2003@users.noreply.github.com>
2023-07-13 17:45:28 +0530

    Linear_Search.java

--- src/basic_array_problems_1/Linear_Search.java
+++ src/basic_array_problems_1/Linear_Search.java
@@ -1,7 +1,21 @@
 package basic_array_problems_1;
 
 import java.util.Scanner;
+/**
+Given an array A and an integer B, find the number of occurrences of B in A.
 
+Input
+	A = [1, 2, 2]
+	B = 2 
+Output
+	2
+
+Input
+	A = [1, 2, 1]
+	B = 3 
+Output
+	0
+*/
 public class Linear_Search {
 
 	private static int linear_search(int[] array, int element) {
